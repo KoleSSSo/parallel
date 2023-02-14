78cc0496c68face28078b7e9527a148c3c1cea12
KoleSSSo <44514859+KoleSSSo@users.noreply.github.com>
2023-02-14 13:42:04 +0700

    Create array.c

---
+++ array.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define _USE_MATH_DEFINES
+#include <malloc.h>
+#include <stdio.h>
+#include <math.h>
+
+#define TYPE float
+#define ABSBASE 100000
+
+int main() {
+	TYPE* a = (TYPE*)malloc(ABSBASE * sizeof(TYPE));
+	TYPE sum = 0;
+#pragma acc kernels
+	for (int i = 1; i <= ABSBASE; i++) {
+		a[i] = sin(i * M_PI * 2 / ABSBASE);
+	}
+#pragma acc kernels 
+	for (int i = 1; i <= ABSBASE; i++) {
+		sum += a[i];
+		}
+	return 0;
+}
